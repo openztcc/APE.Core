9d0b6e6ba4698ecec4f314d5c056969b6f8b95c9
Eric Galvan <lericgalvan@outlook.com>
2025-02-19 22:36:34 -0800

    Updated ApeExp.cpp for cross platform compatibility

--- bindings/ApeExp.cpp
+++ bindings/ApeExp.cpp
@@ -4,18 +4,24 @@
 // Licensed under MIT (see LICENSE)
 //
 // Python bindings for APE.CORE
-// Version 0.2.0
+// Version 0.4.0
 
 #include "../ApeCore.h"
 
-#define VERSION "0.3.0"
+#define VERSION "0.4.0"
+
+#ifdef _WIN32
+    #define EXPORT __declspec(dllexport)
+#else
+    #define EXPORT __attribute__((visibility("default")))
+#endif
 
 extern "C" {
-    __declspec(dllexport) ApeCore* create_ape_instance() {
+    EXPORT ApeCore* create_ape_instance() {
         return new ApeCore();
     }
 
-    __declspec(dllexport) void destroy_ape_instance(ApeCore* ape) {
+    EXPORT void destroy_ape_instance(ApeCore* ape) {
         if (ape) {
             // Ensure OutputBuffer pointers are cleared before deletion
             for (int i = 0; i < ape->getFrameCount(); i++) {
@@ -25,56 +31,56 @@ extern "C" {
         }
     }
 
-    __declspec(dllexport) int load_image(ApeCore* ape, const char* fileName, int colorProfile, const char* palName) {
+    EXPORT int load_image(ApeCore* ape, const char* fileName, int colorProfile, const char* palName) {
         if (!ape) return 0;
         return ape->load(std::string(fileName), colorProfile, std::string(palName)) ? 1 : 0;
     }
 
-    __declspec(dllexport) int get_frame_count(ApeCore* ape) {
+    EXPORT int get_frame_count(ApeCore* ape) {
         return (ape) ? ape->getFrameCount() : 0;
     }
 
-    __declspec(dllexport) OutputBuffer** get_frame_buffer(ApeCore* ape) {
+    EXPORT OutputBuffer** get_frame_buffer(ApeCore* ape) {
         if (!ape || ape->getFrameCount() == 0) return nullptr;
         return ape->apeBuffer();
     }
 
-    __declspec(dllexport) void free_frame_buffer(OutputBuffer** buffer) {
+    EXPORT void free_frame_buffer(OutputBuffer** buffer) {
         if (buffer) {
             delete[] buffer;
         }
     }
 
-    __declspec(dllexport) Color* get_colors(ApeCore* ape) {
+    EXPORT Color* get_colors(ApeCore* ape) {
         if (!ape || ape->getColors().empty()) 
             return nullptr;
         return ape->getColors().data();
     }
 
-    __declspec(dllexport) Frame* get_frames(ApeCore* ape) {
+    EXPORT Frame* get_frames(ApeCore* ape) {
         if (!ape || ape->getFrames().empty()) 
             return nullptr;
         return ape->getFrames().data();
     }
 
-    __declspec(dllexport) int frame_to_png(ApeCore* ape, const char* fileName, int index) {
+    EXPORT int frame_to_png(ApeCore* ape, const char* fileName, int index) {
         if (!ape || index < 0 || index >= ape->getFrameCount()) return 0;
         return ape->exportToPNG(std::string(fileName), *ape->apeBuffer()[index]) ? 1 : 0;
     }
 
-    __declspec (dllexport) int validate_graphic_file(const char* fileName) {
+    EXPORT int validate_graphic_file(const char* fileName) {
         return ApeCore::validateGraphicFile(std::string(fileName));
     }
 
-    __declspec (dllexport) int validate_palette_file(const char* fileName) {
+    EXPORT int validate_palette_file(const char* fileName) {
         return ApeCore::validatePaletteFile(std::string(fileName));
     }
 
-    __declspec (dllexport) int has_background_frame(const char* fileName) {
+    EXPORT int has_background_frame(const char* fileName) {
         return ApeCore::hasBackgroundFrame(std::string(fileName));
     }
 
-    __declspec (dllexport) Header get_header(const char* fileName) {
+    EXPORT Header get_header(const char* fileName) {
         return ApeCore::getHeader(std::string(fileName));
     }
 }
