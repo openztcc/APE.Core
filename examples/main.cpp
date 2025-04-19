a12ea01965f4c24feb429b2092cb13a1550e9d78
Eric Galvan <lericgalvan@outlook.com>
2025-04-18 20:41:04 -0700

    Add benchmark tests to example code

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,7 +1,8 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "../ApeCore.h"
+#include "../include/ApeCore.h"
 #include <string>
 #include <iostream>
+#include <chrono>
 
 int main(int argc, char const *argv[])
 {
@@ -21,34 +22,51 @@ int main(int argc, char const *argv[])
     std::cout << "Opening " << input_file << std::endl;
     std::cout << "Using palette " << palette_file << std::endl;
 
+    // Start test 
+    double total_time = 0.0;
+    auto start = std::chrono::high_resolution_clock::now();
+
     if (argc > 2) {
         // load custom palette
         if (core.load(input_file, 0, palette_file)) {
-            std::cout << "loaded" << std::endl;
+            // std::cout << "loaded" << std::endl;
         } else {
-            std::cout << "failed to load" << std::endl;
+            std::cout << "Failed to decode " << input_file << std::endl;
+            return 1;
         }
     } 
 
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> runtime = end - start;
+    total_time += runtime.count();
+    std::cout << "Time taken to decode " << core.getFrameCount() << " frames: " << total_time << " seconds" << std::endl;
+    std::cout << "Average time per frame: " << total_time / core.getFrameCount() << " seconds" << std::endl;
 
     int numBuffers = core.getFrameCount();
-    std::cout << "numBuffers: " << numBuffers << std::endl;
+    // std::cout << "numBuffers: " << numBuffers << std::endl;
     std::cout << "Writing PNGs" << std::endl;
+    int successCount = 0;
     for (int i = 0; i < numBuffers; i++) {
-        core.exportToPNG(output_name + std::to_string(i) + ".png", *core.apeBuffer()[i]);
+        int error = core.exportToPNG(output_name + std::to_string(i) + ".png", *core.apeBuffer()[i]);
+        if (error < 0) {
+            std::cout << "Failed to write frame " << i << " to png" << std::endl;
+        } else {
+            successCount++;
+        }
     }
+    std::cout << "Finished writing " << successCount << "frames to PNG with " << numBuffers - successCount << " errors." << std::endl;
 
-    if (ApeCore::validateGraphicFile(input_file)) {
-        std::cout << "Valid graphic file" << std::endl;
-    } else {
-        std::cout << "Invalid graphic file" << std::endl;
-    }
+    // if (ApeCore::validateGraphicFile(input_file)) {
+    //     std::cout << "Valid ZT graphic file" << std::endl;
+    // } else {
+    //     std::cout << "Invalid ZT graphic file" << std::endl;
+    // }
 
-    if (ApeCore::validatePaletteFile(palette_file)) {
-        std::cout << "Valid palette file" << std::endl;
-    } else {
-        std::cout << "Invalid palette file" << std::endl;
-    }
+    // if (ApeCore::validatePaletteFile(palette_file)) {
+    //     std::cout << "Valid ZT palette file" << std::endl;
+    // } else {
+    //     std::cout << "Invalid ZT palette file" << std::endl;
+    // }
     
     return 0;
 }
